d04c8835033fbefeef19e40693bd3c41ec6e9d26
Duong Tuan <tuan.duong@enouvo.com>
2018-10-12 23:55:33 +0700

    Challenge Linear Regression 1

---
+++ Mid-test_2018:10:09/testMidle.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  9 09:35:26 2018
+
+@author: Admin
+"""
+
+import numpy as np 
+import matplotlib.pyplot as plt
+
+# height (cm)
+X = np.array([[1,2,3,4,5]]).T
+# weight (kg)
+y = np.array([[ 6,7,4,3,2]]).T
+# Visualize data 
+# Building Xbar 
+one = np.ones((X.shape[0], 1))
+Xbar = np.concatenate((one, X), axis = 1)
+
+# Calculating weights of the fitting line 
+A = np.dot(Xbar.T, Xbar)
+b = np.dot(Xbar.T, y)
+w = np.dot(np.linalg.pinv(A), b)
+print('w = ', w)
