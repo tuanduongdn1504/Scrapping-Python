02ce4b34fd59eb0b084798da79981f57ed8d1b8e
Duong Tuan <tuan.duong@enouvo.com>
2018-12-14 20:47:54 +0700

    UPDATE RnTest

--- RnTest_2018:10:16/passDataFromChild/App.js
+++ RnTest_2018:10:16/passDataFromChild/App.js
@@ -6,24 +6,23 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import App from './src/Setup';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    'Shake or press menu button for dev menu'
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+export default class AppCover extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <App />
       </View>
     );
   }
@@ -34,16 +33,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
--- RnTest_2018:10:16/passDataFromChild/index.js
+++ RnTest_2018:10:16/passDataFromChild/index.js
@@ -1,18 +1,7 @@
 /** @format */
-// import React from 'react';
-// import { AppRegistry, View } from 'react-native';
-// import Setup from './src/Setup';
-// import { name as appName } from './app.json';
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <View>
-//         <Setup />
-//       </View>
-//     );
-//   }
-// }
-// AppRegistry.registerComponent(appName, () => App);
-import App from './src/Setup';
 
-const app = new App();
+import { AppRegistry } from 'react-native';
+import AppCover from './App';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => AppCover);
